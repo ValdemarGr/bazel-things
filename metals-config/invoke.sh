ef63b1cefafa9ba8bdb4c856ba01117b0192450b
Valdemar Grange <randomvald0069@gmail.com>
2021-06-17 16:39:10 +0200

    Update invoke.sh

--- metals-config/invoke.sh
+++ metals-config/invoke.sh
@@ -6,4 +6,4 @@ COURSIER=$(coursier fetch -q org.scala-lang:scala-compiler:$2)
 
 DEPS=$(cd $4 && bazel query "deps(//$1:all)" --output location | grep -E '.\.jar$' | grep maven | sed 's/BUILD:[0-9]*:[0-9]*: source file @maven\/\/://')
 
-echo $DEPS | python3 $RUNFILES --name $1 --path $(pwd) --home $HOME --ver $2 --compiler \'$COURSIER\' > $3
+echo $DEPS | python3 $RUNFILES --name $1 --path $(pwd) --home $HOME --ver $2 --compiler "$COURSIER" > $3
