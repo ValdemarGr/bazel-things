214009b9f62ce9638832aef2bdd7d36b0b71cb54
Valdemar Grange <randomvald0069@gmail.com>
2021-06-17 13:30:05 +0200

    Update invoke.sh

--- metals-config/invoke.sh
+++ metals-config/invoke.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-RUNFILES=${BASH_SOURCE[0]}.runfiles/__main__/metals-config
+LOOK=${BASH_SOURCE[0]}.runfiles
+
+RUNFILES=$(find $LOOK -name "write_bloop_config.py" | head -n 1)
 
 DEPS=$(cd $4 && bazel query "deps(//$1:all)" --output location | grep -E '.\.jar$' | grep maven | sed 's/BUILD:[0-9]*:[0-9]*: source file @maven\/\/://')
 
