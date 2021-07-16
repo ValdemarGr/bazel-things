f1687bba6a9b5c8a4051cb7415b791beb31c34d3
Valdemar Grange <randomvald0069@gmail.com>
2021-07-16 19:01:21 +0200

    Update invoke.sh

--- metals-config/invoke.sh
+++ metals-config/invoke.sh
@@ -2,7 +2,7 @@
 LOOK=${BASH_SOURCE[0]}.runfiles
 
 RUNFILES=$(find $LOOK -name "write_bloop_config.py" | head -n 1)
-COURSIER=$(coursier fetch -q org.scala-lang:scala-compiler:$2)
+COURSIER=$(coursier fetch -q org.scala-lang:scala-compiler:$2 | grep scala)
 
 DEPS=$(cd $4 && bazel query "deps(//$1:all)" --output location | grep -E '.\.jar$' | grep maven | sed 's/BUILD:[0-9]*:[0-9]*: source file @maven\/\/://')
 
