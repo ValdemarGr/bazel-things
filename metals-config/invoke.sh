4834cee0577a6af66a209eb9b4f545c37bcad9be
Valdemar Grange <randomvald0069@gmail.com>
2021-08-21 14:18:57 +0200

    Delete invoke.sh

--- metals-config/invoke.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-LOOK=${BASH_SOURCE[0]}.runfiles
-
-RUNFILES=$(find $LOOK -name "write_bloop_config.py" | head -n 1)
-COURSIER=$(coursier fetch -q org.scala-lang:scala-compiler:$2 | grep scala)
-
-python3 $RUNFILES --name $1 --path $4 --home $HOME --ver $2 --compiler "$COURSIER" > $3
