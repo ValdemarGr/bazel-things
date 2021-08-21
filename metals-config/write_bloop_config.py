a4721643c588a988ddf3bd5baa76c46920047efe
Valdemar Grange <randomvald0069@gmail.com>
2021-08-21 14:17:56 +0200

    Update write_bloop_config.py

--- metals-config/write_bloop_config.py
+++ metals-config/write_bloop_config.py
@@ -1,37 +1,50 @@
+#!/usr/bin/env python3
 import argparse
 import json
 import sys
 import subprocess
+import os
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--name", type=str)
-parser.add_argument("--path", type=str)
-parser.add_argument("--home", type=str)
-parser.add_argument("--ver", type=str)
-parser.add_argument("--compiler", type=str)
 
 args = parser.parse_args()
+path = os.getcwd()
 
-def scala_paths(path):
-    lines = subprocess.Popen(["bash", "-c", f"""cd {path} && bazel query "deps(...)" --output location | rg "/[^ ]+scala_project_[^/]+" -o | uniq"""], cwd=path, stdout=subprocess.PIPE).stdout.readlines()
+def query_bazel_maven_deps():
+    lines = subprocess.Popen(["bash", "-c", "bazel query \"@maven//:all\" --output=build | grep maven_coordinates | sed 's/.*tes=//' | sed 's/\"].*//'"], stdout=subprocess.PIPE).stdout.readlines()
+    formatted = [x.decode("utf-8").strip() for x in lines]
+    print(f"queried {len(formatted)} deps from bazel", file=sys.stderr)
+    return formatted
+
+def fetch_deps(maven_strings, with_sources=True):
+    e = ""
+    if with_sources:
+        e = f"--sources"
+    sane_strings = " ".join(maven_strings)
+    lines = subprocess.Popen(["bash", "-c", f"coursier fetch --default=true {e} {sane_strings}"], stdout=subprocess.PIPE).stdout.readlines()
+    formatted = [x.decode("utf-8").strip() for x in lines]
+    print(f"found {len(formatted)} jar + source items", file=sys.stderr)
+    return formatted
+
+def scala_paths(magic_string):
+    lines = subprocess.Popen(["bash", "-c", f"""cd {path} && bazel query "deps(...)" --output location | rg "/[^ ]+{magic_string}[^/]+" -o | uniq"""], cwd=path, stdout=subprocess.PIPE).stdout.readlines()
     scala_output = [x.decode("utf-8").strip() for x in lines if x.decode("utf-8").strip() != path]
     #dont recurse, that doesnt make sense. All deps are declared locally
     #return scala_output + [x for next in scala_output for x in scala_paths(next)]
     return scala_output
 
-def go(sps):
-    for sp in sps:
-        cmd = ["bash", "-c", f"""cd {sp} && bazel query "deps(...)" --output location | grep -E '.\.jar$' | grep maven | sed 's/BUILD:[0-9]*:[0-9]*: generated file @maven\/\/://'"""]
-        lines = subprocess.Popen(cmd, cwd=sp, stdout=subprocess.PIPE).stdout.readlines()
-        dep_output = [x.decode("utf-8").strip() for x in lines]
-        yield from dep_output
+bazel_deps = query_bazel_maven_deps()
+maven_deps = fetch_deps(bazel_deps)
+rec_paths = list(set(scala_paths("scala_project_")))
+asLst = list(maven_deps)
 
-rec_paths = list(set(scala_paths(args.path)))
-asLst = list(set(go([args.path] + rec_paths)))
+absPath = path + "/" + args.name
 
-absPath = args.path + "/" + args.name
-
-comp = args.compiler
+version = [x for x in bazel_deps if "scala-compiler" in x][0].split(":")[2]
+only_comp = fetch_deps([f"org.scala-lang:scala-compiler:{version}"], with_sources=False)
+comp = [x for x in only_comp if "scala" in x]
+print(f"using compiler items {comp}", file=sys.stderr)
 
 sources = list(filter(lambda x: x.endswith("sources.jar"), asLst))
 nonSources = list(filter(lambda x: not x.endswith("sources.jar"), asLst))
@@ -68,17 +81,7 @@ def modularize(dep):
 def first_or(s, d):
     return next((l for l in nonSources if s in l and False), d)
 
-def mk_def(l):
-    s = args.home + "/.sbt/boot/scala-" + args.ver + "/lib/" + l + ".jar"
-    return first_or(l, s)
-
 found_plugins = [x for x in nonSources for p in plugins if p in x]
-defas = [
-    mk_def("scala-reflect"),
-    mk_def("scala-compiler"),
-    mk_def("scala-library"),
-]
-
 
                               
 out = {
@@ -88,14 +91,14 @@ out = {
         "scala": {
             "organization": "org.scala-lang",
             "name": "scala-compiler",
-            "version": args.ver,
+            "version": version,
             "options":[
                 f"-Xplugin:{p}" for p in found_plugins
                 ],
-            "jars": comp.split()
+            "jars": comp
         },
         "directory" : absPath,
-        "workspaceDir" : args.path,
+        "workspaceDir" : path,
         "sources" : [
             absPath + "/src/main/scala",
             absPath + "/main/scala",
@@ -103,9 +106,9 @@ out = {
             absPath + "/test/scala"
         ] + [x + "/src/main/scala" for x in rec_paths] + [x + "/src/test/scala" for x in rec_paths],
         "dependencies":[],
-        "classpath": nonSources + list(filter(lambda x: "scala-library" in x, comp.split())),
-        "out": args.path + "/.bloop/" + args.name,
-        "classesDir": args.path + "/.bloop/" + args.name + "/scala-" + ".".join(args.ver.split(".")[:-1]) + "/classes",
+        "classpath": nonSources + list(filter(lambda x: "scala-library" in x, comp)),
+        "out": path + "/.bloop/" + args.name,
+        "classesDir": path + "/.bloop/" + args.name + "/scala-" + ".".join(version.split(".")[:-1]) + "/classes",
         "resolution": {
             "modules": list(map(lambda x: modularize(x), nonSources)) 
         },
@@ -113,4 +116,4 @@ out = {
     }
 }
 
-print(json.dumps(out))
+print(json.dumps(out, indent=4, sort_keys=True))
