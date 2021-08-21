10f963905e38fa3c8d3bc18e8f7df94041a9d096
Valdemar Grange <randomvald0069@gmail.com>
2021-08-21 14:16:59 +0200

    Update doze.sh

--- doze/doze.sh
+++ doze/doze.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-CMDS=$($@ 2>&1 > /dev/null | grep "buildozer 'remove")
+echo "running $@"
+CMDS=$(eval $@ 2>&1 > /dev/null | grep "buildozer 'remove")
 while IFS= read -r line; do
   eval "$line"
-  echo "evalling $line"
+  echo "evalling ${line/_2_[0-9]*/""}"
 done <<< "$CMDS"
