8a1e7a71b9733dd5dfab265acc533ccdda4799fc
ValdemarGr <randomvald0069@gmail.com>
2021-12-09 22:26:13 +0100

    understand generated jars

--- metals-config/write_bloop_config.py
+++ metals-config/write_bloop_config.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 import argparse
 import json
+from pathlib import Path
 import sys
 import subprocess
 import os
@@ -11,6 +12,24 @@ parser.add_argument("--path", type=str)
 
 args = parser.parse_args()
 path = os.getcwd()
+
+# scan for generated code
+scanned_jars = []
+scanned_src_jars = []
+p = Path('./bazel-bin')
+if p.is_dir():
+    # everything else than external is interesting
+    d = p
+    paths = [x for x in p.iterdir() if (p / 'external') != x and d != x]
+
+    all_jars = [str(x.resolve()) for path in paths for x in list(path.glob("**/*.jar"))]
+    scanned_src_jars = [x for x in all_jars if x.endswith("src.jar")]
+    scanned_jars = [x for x in all_jars if not x.endswith("src.jar")]
+    # print(scanned_jars)
+    # print(scanned_src_jars)
+    # scanned_jars2 = [x for x in glob.glob(f"{path}/*.jar") for path in paths]
+    # print(scanned_jars)
+    # print(scanned_jars2)
 
 def query_bazel_maven_deps():
     lines = subprocess.Popen(["bash", "-c", "bazel query \"@maven//:all\" --output=build | grep maven_coordinates | sed 's/.*tes=//' | sed 's/\"].*//'"], stdout=subprocess.PIPE).stdout.readlines()
@@ -68,6 +87,20 @@ nonSources = list(filter(lambda x: not x.endswith("sources.jar"), asLst))
 
 plugins = set(["kind-projector", "better-monadic-for"])
 
+def make_artifact(org, pkgName, version, artifacts):
+    return {
+            "organization": org,
+            "name": pkgName,
+            "version": version,
+            "configurations": "default",
+            "artifacts": artifacts
+    }
+
+def make_scanned():
+    comp = [{"name": str(i), "path": j} for (i, j) in enumerate(scanned_jars)]
+    srcs = [{"name": str(i), "path": j, "classifier": "sources"} for (i, j) in enumerate(scanned_src_jars)]
+    return make_artifact("scanned_jars", "scanned_jars", "1.0.0", comp + srcs)
+
 def modularize(dep):
     toSearch = "maven2/"
     strippedStr = dep[dep.find(toSearch)+len(toSearch):]
@@ -77,23 +110,35 @@ def modularize(dep):
     pkgName = rest[rest.rfind("/") + 1:]
     org = rest[:rest.rfind("/")].replace("/", ".")
 
-    return {
-            "organization": org,
+    return make_artifact(org, pkgName, version, [
+        {
             "name": pkgName,
-            "version": ver,
-            "configurations": "default",
-            "artifacts": [
-                {
-                    "name": pkgName,
-                    "path": dep
-                },
-                {
-                    "name": pkgName,
-                    "classifier": "sources",
-                    "path": dep[:-4] + "-sources.jar"
-                }
-            ]
-    }
+            "path": dep
+        },
+        {
+            "name": pkgName,
+            "classifier": "sources",
+            "path": dep[:-4] + "-sources.jar"
+        }
+    ])
+
+    # return {
+    #         "organization": org,
+    #         "name": pkgName,
+    #         "version": ver,
+    #         "configurations": "default",
+    #         "artifacts": [
+    #             {
+    #                 "name": pkgName,
+    #                 "path": dep
+    #             },
+    #             {
+    #                 "name": pkgName,
+    #                 "classifier": "sources",
+    #                 "path": dep[:-4] + "-sources.jar"
+    #             }
+    #         ]
+    # }
 
 def first_or(s, d):
     return next((l for l in nonSources if s in l and False), d)
@@ -123,11 +168,11 @@ out = {
             absPath + "/test/scala"
         ] + rec_paths,
         "dependencies":[],
-        "classpath": nonSources + list(filter(lambda x: "scala-library" in x, comp)),
+        "classpath": nonSources + list(filter(lambda x: "scala-library" in x, comp)) + scanned_jars,
         "out": path + "/.bloop/" + args.path,
         "classesDir": path + "/.bloop/" + args.path + "/scala-" + ".".join(version.split(".")[:-1]) + "/classes",
         "resolution": {
-            "modules": list(map(lambda x: modularize(x), nonSources)) 
+            "modules": list(map(lambda x: modularize(x), nonSources)) + [make_scanned()]
         },
         "tags": ["library"]
     }
